eae12b688a3187cdd8eef33e85a64473529e57fc
csyanzen <csyanzen@gmail.com>
2012-12-10 18:20:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makan::Application.config.secret_token = '65b785c745c96987640f2f5adf4fbff96eb9d71188b2ab6c33d9f8b58ad90791d93076cead4f21a5ada9fa308e2302fd48667dbc7e49b64aea852de56b8329a1'
